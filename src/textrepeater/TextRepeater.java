c6a3f52f55c124518cacd9c16bbf52c408eece8f
ahmed seada <ahmedm.seada@gmail.com>
2018-02-19 23:06:59 +0200

    First Commit

---
+++ src/textrepeater/TextRepeater.java
@@ -0,0 +1,20 @@
+/*
+ * Created :  By Ahmed Seada.
+
+ */
+package textrepeater;
+
+/**
+ *
+ * @author ahmed
+ */
+public class TextRepeater {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new form1().setVisible(true);
+    }
+    
+}
